6738a964ea831d29a0c9653e107cbd6db2542ac3
Steve KN6ORM <kn6orm@gmail.com>
2021-07-30 01:07:22 -0700

    capture using USB camera

--- src/meshcam.py
+++ src/meshcam.py
@@ -1,18 +1,20 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import cv2
 import os
 import shutil
 import subprocess
 import tempfile
 import time
 
-hostName = ""
-hostPort = 80
+host_name = ""
+host_port = 80
+source = 0
+cv2cap = None
 
 dir = tempfile.mkdtemp()
 cap_file = os.path.join(dir, 'cam.jpg')
 
-
-def get_image(file):
+def get_picam_image(file):
     cmd = [ '/usr/bin/raspistill' ]
     cmd.append('-n')        # no ui
     cmd.append('-q')
@@ -26,13 +28,22 @@ def get_image(file):
     res = subprocess.run(cmd)
     return res.returncode == 0
 
-class MyServer(BaseHTTPRequestHandler):
+def get_usbcam_image():
+    ret, frame = cv2cap.read()
+    if not ret:
+        return None
+    return cv2.imencode('.jpg', frame)[1].tobytes()
+
+class MeshCam(BaseHTTPRequestHandler):
     def do_GET(self):
         image = None
-        if get_image(cap_file):
-            with open(cap_file, mode='rb') as file:
-                image = file.read()
-            os.unlink(cap_file)
+        if source == None:
+            if get_picam_image(cap_file):
+                with open(cap_file, mode='rb') as file:
+                    image = file.read()
+                os.unlink(cap_file)
+        else:
+            image = get_usbcam_image()
 
         if image is not None:
             self.send_response(200)
@@ -42,14 +53,20 @@ class MyServer(BaseHTTPRequestHandler):
         else:
             self.send_error(500)
 
-myServer = HTTPServer((hostName, hostPort), MyServer)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+if __name__ == '__main__':
+    server = HTTPServer((host_name, host_port), MeshCam)
+    print(time.asctime(), "Server Starts - %s:%s" % (host_name, host_port))
+    if source is not None:
+        cv2cap = cv2.VideoCapture(source)
 
-try:
-    myServer.serve_forever()
-except KeyboardInterrupt:
-    pass
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+    if cv2cap is not None:
+        cv2cap.release()
+
+    server.server_close()
+    print(time.asctime(), "Server Stops - %s:%s" % (host_name, host_port))
 
